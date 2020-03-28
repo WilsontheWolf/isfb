613616993167c86ca17ef1dad907929c77fcb251
Glitch (isfb) <none>
2020-03-28 21:21:30 +0000

    😼🏝 Checkpoint ./events/message.js:712571/42

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const disallowedChannels = ['516624854066135050'];
-const tech = ["tech", "forge"];
-const test = ["test update", "beta version", "tester", "test version"];
-const endless = ["endless", "end less"];
-const sandbox = ["sandbox", "sand box", "creative"]
+const tech = [/*"tech", "forge"*/];
+const test = [/*"test update", "beta version", "tester", "test version"*/];
+const endless = [/*"endless", "end less"*/];
+const sandbox = [/*"sandbox", "sand box", "creative"*/]
 const responce = async (msg, id, client) => {
   if (msg.author.id == id) return;
   if (msg.channel.id == "674937391529590784" || msg.channel.id == '516624854066135050') return;
