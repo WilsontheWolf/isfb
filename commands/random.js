a556d61353bec4cb77860d86953870f5e6ed6e18
Glitch (isfb) <none>
2020-05-19 01:07:52 +0000

    🎆🕰 Checkpoint ./commands/random.js:712571/5724

---
+++ commands/random.js
@@ -0,0 +1,25 @@
+exports.run = async (client, message, args, level) => {
+  let black = client.cards.filter(c => c.type == 'black').random() 
+  let white = client.cards.filter(c => c.type == 'white').random()
+  let bName = `Unknown User (${black.owner})`
+  if(client.users.has(black.owner)) bName = client.users.get(black.owner).username
+  let wName = `Unknown User (${white.owner})`
+  if(client.users.has(black.owner)) wName = client.users.get(white.owner).username
+  message.channel.send(`Black submitted by ${bName}: ${black.value}
+White submitted by ${wName}: ${white.value}`)  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['rand', 'randomcard', 'iajrandom'],
+  permLevel: "User",
+  hidden: false
+};
+
+exports.help = {
+  name: "random",
+  category: "Islands Against Jwiggs",
+  description: "Get a random pair of black and white IIslands Against Jwiggs Cards:tm:.",
+  usage: "random"
+};
