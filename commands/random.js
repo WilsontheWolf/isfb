eb9461b3970aa6e5e6638f44e6711f140639199c
Glitch (isfb) <none>
2020-05-19 05:52:52 +0000

    🎐🍿 Checkpoint ./commands/random.js:712571/12

--- commands/random.js
+++ commands/random.js
@@ -23,7 +23,7 @@ exports.run = async (client, message, args, level) => {
   }
   let bName = `Unknown User (${black.owner})`
   if(client.users.has(black.owner)) bName = client.users.get(black.owner).username
-  message.author.send(`${black.value}
+  message.cgabbe.send(`${black.value}
 Black submitted by ${bName}. White submitted by ${white[0] ? white.map(w => w.name).join(', ') : 'noone'}`)  
 };
 
