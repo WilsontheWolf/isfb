44817ed33d91a9acfbeede2a8a0d4ef48cc83990
Glitch (isfb) <none>
2020-05-20 23:05:43 +0000

    ❄️🚘 Checkpoint ./commands/cards.js:712571/420

--- commands/cards.js
+++ commands/cards.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js')
 exports.run = async (client, message, args, level) => {
+  let user = message.author
+  if(args[0]) user = await client.fetchUser(args.join(' '), message)
 const embed = new Discord.RichEmbed()
 .setTitle('Card Stats')
 .addField('Total Cards:', client.cards.count)
 .addField('Black Cards:', client.cards.filter(c => c.type == 'black').size)
 .addField('White Cards:', client.cards.filter(c => c.type == 'white').size)
-.addField('Your Cards:', client.cards.filter(c => c.owner == message.author.id).size)
+.addField(`${user == message.author ? 'Your' : user.username + "'s"} Cards:`, client.cards.filter(c => c.owner == user.id).size)
 message.channel.send(embed)
 };
 
