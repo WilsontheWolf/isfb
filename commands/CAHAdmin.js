afb821abc5c8c10a1c2f1b5c692638f56da37448
Glitch (isfb) <none>
2020-05-24 02:53:04 +0000

    📰☺️ Checkpoint ./commands/CAHAdmin.js:712571/520 ./index.js:712571/165

---
+++ commands/CAHAdmin.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message, args, level) => {
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['cah'],
+  permLevel: "Bot Admin",
+  hidden: true
+};
+
+exports.help = {
+  name: "iaj",
+  category: "Islands Against Jwiggs",
+  description: "Admin commands for the Islands Agains Jwiggs Game.",
+  usage: "ping"
+};
