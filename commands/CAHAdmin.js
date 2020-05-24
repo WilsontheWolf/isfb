56aa4607e53cd4f7c3106b6cc332811864d6477d
Glitch (isfb) <none>
2020-05-24 03:16:15 +0000

    🌽⛑ Checkpoint ./commands/CAHAdmin.js:712571/131

--- commands/CAHAdmin.js
+++ commands/CAHAdmin.js
@@ -1,5 +1,6 @@
 exports.run = async (client, message, args, level) => {
-  
+  if(!args[0]) return message.channel.send('please provide a action.')
+  let action = args[0]
 };
 
 exports.conf = {
@@ -14,5 +15,5 @@ exports.help = {
   name: "iaj",
   category: "Islands Against Jwiggs",
   description: "Admin commands for the Islands Agains Jwiggs Game.",
-  usage: "ping"
+  usage: ""
 };
