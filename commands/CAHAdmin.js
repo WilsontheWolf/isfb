800a0051ae8c0d0e1b47d1160eb92c342a907468
Glitch (isfb) <none>
2020-05-24 07:11:09 +0000

    👑🐿 Checkpoint ./commands/CAHAdmin.js:712571/956

--- commands/CAHAdmin.js
+++ commands/CAHAdmin.js
@@ -8,11 +8,25 @@ exports.run = async (client, message, args, level) => {
   funcs.delete = (id) => {
     if(id == 'count') return message.reply('invalid id.')
     let c = client.cards.get(id)
-    if(!c)
+    if(!c) return message.reply('invalid id.')
     client.cards.delete(id)
     message.reply(`Deleted card \`${id}\` with value \`${c.value}\``)
   }
-  funcs.edit = (id, type, value) => {
+  funcs.edit = (id, type) => {
+    arguments.splice(0, 2)
+    let value = arguments
+    if(id == 'count') return message.reply('invalid id.')
+    let c = client.cards.get(id)
+    if(!c) return message.reply('invalid id.')
+    if(!['value', 'owner', 'colour'].includes(type.toLowerCase())) return message.reply('invalid type.')
+  }
+  const editValue = (id, o, n) => {
+    
+  }
+  const editOwner = (id, o, n) => {
+    
+  }
+  const editColour = (id, o, n) => {
     
   }
 };
