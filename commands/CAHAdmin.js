ee32aa569a8faaf13474f54d55999c624ceaa006
Glitch (isfb) <none>
2020-06-03 23:06:27 +0000

    🐂🌄 Checkpoint ./events/cahCreate.js:712571/1191 ./commands/CAHGame.js:712571/3300 ./commands/CAHAdmin.js:712571/8705

--- commands/CAHAdmin.js
+++ commands/CAHAdmin.js
@@ -1,10 +1,14 @@
 const funcs = {}
-  const Discord = require('discord.js')
+const Discord = require('discord.js')
+let games = client.game
+let cards = client.cards.filter(c => c.type)
+let black = cards.filter(c => c.type == 'black')
+let white = cards.filter(c => c.type == 'white')
 exports.run = async (client, message, args, level) => {
-  funcs.delete = function deleteis(id){
-    if(id == 'count') return message.reply('invalid id.')
+  funcs.delete = function deleteis(id) {
+    if (id == 'count') return message.reply('invalid id.')
     let c = client.cards.get(id)
-    if(!c) return message.reply('invalid id.')
+    if (!c) return message.reply('invalid id.')
     client.cards.delete(id)
     message.reply(`Deleted card \`${id}\` with value \`${c.value}\``)
   }
@@ -13,36 +17,47 @@ exports.run = async (client, message, args, level) => {
     let a = Object.values(arguments)
     a.splice(0, 2)
     let value = a.join(' ')
-    if(id == 'count') return message.reply('invalid id.')
+    if (id == 'count') return message.reply('invalid id.')
     let c = client.cards.get(id)
-    if(!c) return message.reply('invalid id.')
-    if(!['value', 'owner', 'colour'].includes(type)) return message.reply('invalid type.')
-    if(type == 'value') editValue(id, c, value)
-    if(type == 'owner') editOwner(id, c, value)
-    if(type == 'colour') editColour(id, c, value)
+    if (!c) return message.reply('invalid id.')
+    if (!['value', 'owner', 'colour'].includes(type)) return message.reply('invalid type.')
+    if (type == 'value') editValue(id, c, value)
+    if (type == 'owner') editOwner(id, c, value)
+    if (type == 'colour') editColour(id, c, value)
   }
-  funcs.search = function search(){
+  funcs.search = function search() {
     let q = Object.values(arguments)
     let cds = client.cards.filter(c => c.value && c.value.toLowerCase().includes(q.join(' ').toLowerCase()))
     if (!cds.size) return message.reply('no results found.')
     let r = cds.map((c, i) => `${i} ${client.users.has(c.owner) ? client.users.get(c.owner).tag : "Unknown User (" + c.owner + ")"} ${c.type}: ${c.value}`).join('\n')
     const embed = new Discord.RichEmbed()
-    .setTitle('Results')
-    .setDescription(`\`\`\`${r.substr(0, 2041)}${r.length > 2041 ? '…' : ''}\`\`\``)
+      .setTitle('Results')
+      .setDescription(`\`\`\`${r.substr(0, 2041)}${r.length > 2041 ? '…' : ''}\`\`\``)
     message.channel.send(embed)
-    }
-  funcs.info = function info(id){
-    if(id == 'count') return message.reply('invalid id.')
+  }
+  funcs.info = function info(id) {
+    if (id == 'count') return message.reply('invalid id.')
     let c = client.cards.get(id)
     if (!c) return message.reply('no such card.')
     const embed = new Discord.RichEmbed()
-    .setAuthor(`Info for card ${id}`)
-    .setTitle('Content:')
-    .setDescription(c.value)
-    .addField('Owner:', client.users.has(c.owner) ? client.users.get(c.owner).tag : "Unknown User (" + c.owner + ")", true)
-    .addField('Type:', c.type, true)
-    
+      .setAuthor(`Info for card ${id}`)
+      .setTitle('Content:')
+      .setDescription(c.value)
+      .addField('Owner:', client.users.has(c.owner) ? client.users.get(c.owner).tag : "Unknown User (" + c.owner + ")", true)
+      .addField('Type:', c.type, true)
+
     message.channel.send(embed)
+  }
+  func.start = () => {
+    
+    games.set(1, {
+      white: white,
+      black: black,
+      players: {},
+      czar: null,
+      round: 0,
+      state: 'waiting'
+    })
   }
   const editValue = (id, o, n) => {
     o.value = n
@@ -51,23 +66,23 @@ exports.run = async (client, message, args, level) => {
   }
   const editOwner = async (id, o, n) => {
     let user = await client.fetchUser(n, message)
-    if(!user) return message.reply('you must provide a valid user.')
+    if (!user) return message.reply('you must provide a valid user.')
     o.owner = user.id
     client.cards.set(id, o)
     message.reply('successfully set the new value.')
   }
   const editColour = (id, o, n) => {
-    if(!['black', 'white'].includes(n.toLowerCase())) return message.reply('Please choose a valid colour.')
+    if (!['black', 'white'].includes(n.toLowerCase())) return message.reply('Please choose a valid colour.')
     o.type = n
     client.cards.set(id, o)
     message.reply('successfully set the new value.')
   }
-  
-  if(!args[0]) return message.reply('please provide a action.')
+
+  if (!args[0]) return message.reply('please provide a action.')
   let action = funcs[args.shift()]
-  if(!action) return message.reply('invalid action.')
+  if (!action) return message.reply('invalid action.')
   action(...args)
-  
+
 };
 
 exports.conf = {
