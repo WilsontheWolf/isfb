9212d18e8dc1f2e584afe2b4f5138cff0a38c436
Glitch (isfb) <none>
2020-06-04 21:06:50 +0000

    👡📇 Checkpoint ./events/cahStart.js:712571/775

--- events/cahStart.js
+++ events/cahStart.js
@@ -1,5 +1,13 @@
 module.exports = async (client, id) => {
   let games = client.game
   let game = games.get(id)
-  if(!game) return console.warn(`Something tried starting game ${id} but it doesn't exist.`)
+  if (!game) return console.warn(`Something tried starting game ${id} but it doesn't exist.`)
+  game.players.forEach(p => {
+    /*{
+      id,
+      points: 0,
+      cards: [],
+    }*/
+    p.
+  })
 };
