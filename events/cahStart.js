2606a50b3c613ccb810102d41e1c52d873999933
Glitch (isfb) <none>
2020-06-04 19:12:10 +0000

    ❄️🐉 Checkpoint ./events/cahStart.js:712571/132

--- events/cahStart.js
+++ events/cahStart.js
@@ -1,5 +1,5 @@
 module.exports = async (client, id) => {
   let games = client.game
   let game = games.get(id)
-  
+  if(!game) return console.warn(`Something tried starting game ${id} but it doesn't exist.`)
 };
