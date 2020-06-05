474b34e342b8007ba2a05994f5b71d48c90e13b9
Glitch (isfb) <none>
2020-06-05 06:59:53 +0000

    🎎⛪️ Checkpoint ./events/cahStart.js:712571/106 ./events/cahStart copy.js:712571/953 ./events/cahNewRound.js:712571/794

--- events/cahNewRound.js
+++ events/cahNewRound.js
@@ -2,21 +2,11 @@ module.exports = async (client, id) => {
   let games = client.game
   let game = games.get(id)
   if (!game) return console.warn(`Something tried starting game ${id} but it doesn't exist.`)
-  if (game.state != 'waiting') return console.warn(`Something tried starting game ${id} but it isn't waiting.`)
   Object.keys(game.players).forEach(pid => {
-    /*{
-      id,
-      points: 0,
-      cards: [],
-    }*/
-    for (let i = 0; i < 8; i++)
-      game.players[pid].cards.push(getCard(id, client, 'white'))
+      let user = client.users.get(pid)
+      if(!user) return console.error(`Error Finding User ${pid}!!!`)
+      
   })
-  games.set(id, game.players, 'players')
-  games.set(id, 'picking', 'state')
-  let black = getCard(id, client, 'black')
-  games.set(id, black, 'curBlack')
-  client.emit('cahNewRound', id)
 };
 function getCard(id,client, type = 'white') {
   let cards = client.game.get(id, type)
