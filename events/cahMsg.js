2aeb919f30d572e626e3ff3d05f333d47502affa
Glitch (isfb) <none>
2020-06-07 23:25:31 +0000

    👢🍘 Checkpoint ./events/cahMsg.js:712571/805

--- events/cahMsg.js
+++ events/cahMsg.js
@@ -1,25 +1,3 @@
-module.exports = async (client, id) => {
-  let games = client.game
-  let game = games.get(id)
-  if (!game) return console.warn(`Something tried starting game ${id} but it doesn't exist.`)
-  if (game.state != 'waiting') return console.warn(`Something tried starting game ${id} but it isn't waiting.`)
-  Object.keys(game.players).forEach(pid => {
-    /*{
-      id,
-      points: 0,
-      cards: [],
-    }*/
-    for (let i = 0; i < 8; i++)
-      game.players[pid].cards.push(getCard(id, client, 'white'))
-  })
-  games.set(id, game.players, 'players')
-  client.emit('cahNewRound', id)
+module.exports = async (client, msg) => {
+  
 };
-function getCard(id,client, type = 'white') {
-  let cards = client.game.get(id, type)
-  if (!cards) return;
-  let value = client.Rnd(0, cards.length)
-  let card = cards[value]
-  client.game.set(id, cards.filter((c, i) => i != value), type)
-  return card
-}
